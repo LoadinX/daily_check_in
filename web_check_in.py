90b4842917dfae748c34bfbbb19fb331bdd6336b
chidun xu <chidunxu@gmail.com>
2020-05-26 21:59:31 +0800

    add (failed)code for supporting new website[bianhuaho]

--- web_check_in.py
+++ web_check_in.py
@@ -13,15 +13,21 @@ import time
 #browser = webdriver.Edge()
 'edge has updated to chrome-based edge, edge driver needs update'
 
+gmail = 'chidunxu@gmail.com'
+phone = '15901857132'
 common_password = 'XuMinJie19961127'
 
 load = {
     'huaweicloud':{
         'url':'https://www.huaweicloud.com/HDC.Cloud.html',
-        'user_name':'15901857132',
+        'user_name':phone,
         'password':common_password
     },
-
+    'bianhuaho':{
+        'url':'https://bianhuaho.com/auth/login',
+        'user_name':gmail,
+        'password':common_password
+    },
 }
 
 def huaweicloud():
@@ -50,7 +56,7 @@ def huaweicloud():
     
 
     browser.switch_to.window(browser.window_handles[-1])
-    wait.until(EC.presence_of_element_located((By.CLASS_NAME,'button-content'))) # wai还是很有必要的
+    wait.until(EC.presence_of_element_located((By.CLASS_NAME,'button-content'))) # wait还是很有必要的
     print(browser.title)
     if browser.find_elements_by_class_name('button-content')[0].text == '已签到':
         print('signed\n')
@@ -66,5 +72,43 @@ def huaweicloud():
 
     browser.quit()
 
+def bianhuaho():
+    task = 'bianhuaho'
+    print(f'\n--------------------------------{task.upper()}--------------------------------\n')
+
+    ### problem here:
+    ###        if no proxy, it needs too much time, casuing timeout error
+    ###        the v2ray be using, how to setting proxy
+    # chrome
+    browser = common_chrome()
+    wait = WebDriverWait(browser,30)
+
+    browser.get(load[task]['url'])
+    time.sleep(5)
+    browser.refresh()
+    wait.until(EC.presence_of_element_located((By.ID,'email')))
+    print(browser.title)
+    browser.find_element_by_id('email').send_keys(Keys.CONTROL,'a')
+    browser.find_element_by_id('email').send_keys(load[task]['user_name'])
+    browser.find_element_by_id('password').send_keys(load[task]['password'])
+    wait.until(EC.element_to_be_clickable((By.CLASS_NAME,'btn btn-primary btn-lg btn-block login'))).click()
+
+    browser.switch_to.window(browser.window_handles[-1])
+    wait.until(EC.presence_of_element_located((By.ID,'checkin-div')))
+    browser.find_element_by_id('checkin-div').click()
+
+def common_chrome():
+    options = webdriver.ChromeOptions()
+    for arg in ['lang=zh_CN.UTF-8','head-less']:
+        options.add_argument(arg)
+    browser = webdriver.Chrome(options = options)
+    return browser
+
+def log(content):
+    with open('log.txt','a') as a:
+        a.write(f'[{time.asctime(time.localtime(time.time()))}] {content} ')
+    a.close() 
+
 if __name__ == "__main__":
     huaweicloud()
+    #bianhuaho()
